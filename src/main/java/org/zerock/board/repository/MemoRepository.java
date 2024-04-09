36457130cbe2b4909259b7d01452fd5e8e5b081a
10-5\kkw <wldnd211@gmail.com>
2024-04-09 11:34:55 +0900

    쿼리메서드, 네이티브쿼리

--- src/main/java/org/zerock/board/repository/MemoRepository.java
+++ src/main/java/org/zerock/board/repository/MemoRepository.java
@@ -1,7 +1,14 @@
 package org.zerock.board.repository;
 
+import lombok.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.board.entity.Memo;
+
+import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
     // extends JpaRepository<엔티티명, pk타입>
@@ -16,6 +23,54 @@ public interface MemoRepository extends JpaRepository<Memo, Long> {
     // select 작업 : findById(키 타입), getOne(키 타입)
     // update 작업 : save(엔티티 객체)
     // delete 작업 : deleteById(키 타입),  delete(엔티티 객체)
+
+    // 쿼리 메서드는 메서드명 자체가 쿼리문으로 동작한다.
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
+    //List<Memo> 리턴타입 -> 리스트 타입의 객체는 memo
+    // 매개값으로 받은 from부터 to까지 select를 진행하여 리스트로 리턴하는 쿼리메서드
+    
+    
+    Page<Memo> findByMnoBetween(Long from, Long to , Pageable pageable);
+    // Page<Memo> 리턴타입 -> 페이징 타입에 객체는 memo
+    // 매개값으로 받은 from~to 까지 select를 진행하여 페이징 타입으로 리턴하는 쿼리 메서드
+
+    // 예를 들어 10보다 작은 데이터를 삭제한다.
+    void deleteMemoByMnoLessThan(Long num);
+
+
+    // @Query는 순수한 sql 쿼리문으로 작성한다. 단, 테이블명이 아니라 엔티티 명으로 사용함.
+    @Query("SELECT m FROM Memo m ORDER BY  m.mno DESC")
+    List<Memo> getListDesc(); // 내가 만든 메서드 명
+
+
+    // 매개값이 있는 @Query문 : 값 (타입으로 받음)
+    @Query("UPDATE Memo m SET m.memoText = :memoText WHERE m.mno = :mno")
+    int updateMemoText(@Param("mno") Long mno, @Param("memoText") String memoText);
+
+
+    // 매개값이 객체(빈)으로 들어올 경우
+    @Query("UPDATE Memo m SET m.memoText =:#{memoBean.memoText} WHERE m.mno=:#{memoBean.mno}")
+    int updateMemoBean(@Param("memoBean") Memo memo);
+
+    
+    // @Query 메서드로 페이징 처리 해보기 -> 리턴 타입이 Page<Memo>
+    // 쿼리문 두개 쓸때는 value="query"
+    @Query(value="SELECT m FROM Memo m WHERE m.mno > :mno"
+            ,countQuery = "SELECT count(m) FROM Memo m WHERE m.mno > :mno")
+    Page<Memo> getListWithQuery(Long mno, Pageable pageable);
+
+    
+    //DB에 존재하지 않는 값을 처리 해보기 : 예를 들어 날짜
+    @Query(value = "SELECT m.mno, m.memoText, CURRENT_DATE FROM Memo m WHERE m.mno > :mno",
+    countQuery = "SELECT count(m) FROM Memo m WHERE m.mno > :mno") // countQuery 페이징 처리용
+    Page<Object[]> getListWithQueryObject(Long mno, Pageable pageable);
+
+
+    // Native Sql 처리 : DB용 쿼리로 사용하는 기법
+    // 엔티티 대신 테이블명으로 사용함.
+    @Query(value = "SELECT * FROM memo WHERE mno > 0", nativeQuery = true)
+    List<Object[]> getNativeResult();
+
 
 
 
